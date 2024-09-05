dae1f86fd91c8cd1c7fbd2d7674fa4d9316a88b0
Chestha Jain <cheshtajain1712@gmail.com>
2024-09-05 15:51:40 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login"
 
 function App() {
   return <Router>
       <Header />
-      
-
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
     <Route />
-
     </Routes>
-
   </Router>
 }
 
