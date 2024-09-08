541196cc360c37fe02ca9e94846d58eefb910b74
Chestha Jain <cheshtajain1712@gmail.com>
2024-09-08 19:47:25 +0530

    videos, upload route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Footer from './components/Footer';
 import Signup from './components/Signup';
-
+import cideos from './components/Videos';
+import Upload from './components/Upload';
 
 function App() {
   return <Router>
       <Header />
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/videos" element={<Videos />} />
+    <Route path="/upload" element={<Upload />} />
     <Route path="/login" element={<Login />} />
     <Route path="/Signup" element={<Signup />} />
 
